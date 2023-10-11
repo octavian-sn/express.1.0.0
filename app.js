d563a293c7622498bc076eef53baa5dc6d092bd7
Octavian Sulic <octavius1992@gmail.com>
2023-10-11 16:22:39 +0300

    Add store routes.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 require('dotenv').config()
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const storeRouter = require('./routes/store')
 
 const app = express();
 
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/store', storeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/store.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+// Controller modules
+const tea_controller = require('../controllers/teaController');
+const category_controller = require('../controllers/categoryController');
+
+/* GET store home page. */
+router.get('/', tea_controller.index);
+
+// Tea routes
+router.get('/tea/create', tea_controller.tea_create_get);
+
+router.post('/tea/create', tea_controller.tea_create_post);
+
+router.get('/tea/:id/delete', tea_controller.tea_delete_get);
+
+router.get('/tea/:id/delete', tea_controller.tea_delete_post);
+
+router.get('/tea/:id/update', tea_controller.tea_delete_get);
+
+router.get('/tea/:id/update', tea_controller.tea_delete_post);
+
+router.get('/tea/:id', tea_controller.tea_detail);
+
+router.get('/tea', tea_controller.tea_list);
+
+//Category routes
+router.get('/category/create', category_controller.category_create_get);
+
+router.post('/category/create', category_controller.category_create_post);
+
+router.get('/category/:id/delete', category_controller.category_delete_get);
+
+router.get('/category/:id/delete', category_controller.category_delete_post);
+
+router.get('/category/:id/update', category_controller.category_delete_get);
+
+router.get('/category/:id/update', category_controller.category_delete_post);
+
+router.get('/category/:id', category_controller.category_detail);
+
+router.get('/category', category_controller.category_list);
+
+module.exports = router;
