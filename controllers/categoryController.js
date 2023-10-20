b5156af074075c2f98ab7b3328f37ef57ebf65f1
Octavian Sulic <octavius1992@gmail.com>
2023-10-20 16:39:42 +0300

    Add categoryController's delete get & post methods.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -89,11 +89,25 @@ exports.category_create_post = [
 ]
 
 exports.category_delete_get = asyncHandler(async(req, res, next)=> {
-    res.send(`Not implemented <b>YET</b>: category_delete_get.`)
+    const [ category, tea_list ] = await Promise.all([
+        Category.findById(req.params.id),
+        Tea.find({category: req.params.id})
+            .sort({ title: 1})
+            .exec()
+    ])
+
+    res.render('category_delete', {
+        title: 'Delete Category',
+        head: 'head',
+        sidebar: 'sidebar',
+        category,
+        tea_list,
+    })
 })
 
 exports.category_delete_post = asyncHandler(async(req, res, next)=> {
-    res.send(`Not implemented <b>YET</b>: category_delete_post.`)
+    await Category.findByIdAndRemove(req.body.categoryid);
+    res.redirect('/store/categories');
 })
 
 exports.category_update_get = asyncHandler(async(req, res, next)=> {
